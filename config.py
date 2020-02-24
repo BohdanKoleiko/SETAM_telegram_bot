e765b14dee5053a4a8cdbbb283a6a01c791aa6a7
HamaHs <Lusuy1995@gmail.com>
2020-02-24 15:58:12 +0200

    to change config

--- config.py
+++ config.py
@@ -8,35 +8,35 @@ START_KEYBOARD = dict(
 
 WhatIsSetam = dict(
     Bidding='➡️Взяти участь у торгах',
-    FinanIss='Фінансові питання',
-    VoluntarySale='Добровільний продаж',
-    FreeTransfer='Безоплатна передача',
-    Familiarization='Подати заяву на ознайомлення з майном'
+    FinanIss='➡️Фінансові питання',
+    VoluntarySale='➡️Добровільний продаж',
+    FreeTransfer='➡️Безоплатна передача',
+    Familiarization='➡️Подати заявку на ознайомлення з майном'
 )
 
 
 Bidding = dict(
     Registers='➡️Реєстрація на сайті',
-    ApplyForAuction='Подати заявку на Аукціон'
+    ApplyForAuction='➡️Подати заявку на Аукціон'
 )
 
 Registers = dict(
     RegForIndividuals='➡️Реєстрація для фізичних осіб',
-    RegForLegalEntities='Реєстрація для юридичних осіб',
-    EdPersonalInfo='Редагування особистих даних',
-    ProblemsWithReg='Проблеми з реєстрацією'
+    RegForLegalEntities='➡️ Реєстрація для юридичних осіб',
+    EdPersonalInfo='➡️ Редагування особистих даних',
+    ProblemsWithReg='➡️ Проблеми з реєстрацією'
 )
 
 
 FinanIss = dict(
-    Individuals='Для фізичних осіб',
-    Entities='Для юридичних осіб',
-    Remuneration='Додаткова Винагорода',
-    WarrantyСontribution='Повернення гарантійного внеску',
-    ReasonsForReturn='Підстави повернення гарантійного внеску'
+    Individuals='➡️Для фізичних осіб',
+    Entities='➡️Для юридичних осіб',
+    Remuneration='➡️Додаткова Винагорода',
+    WarrantyСontribution='➡️Повернення гарантійного внеску',
+    ReasonsForReturn='➡️Підстави повернення гарантійного внеску'
 )
 
 
 OtherServicesSetam = dict(
-    Marriage='Шлюб за добу'
+    Marriage='➡️Шлюб за добу'
 )
