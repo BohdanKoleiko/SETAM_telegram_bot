59b5360e075f5b16bcaac765a8dd4a2b650dcec5
HamaHs <Lusuy1995@gmail.com>
2020-03-12 10:26:46 +0000

    test

---
+++ key_but.py
@@ -0,0 +1,40 @@
+START_KEYBOARD = dict(
+    WhatIsSetam="➡️Питання щодо онлайн-аукціонів",
+    OtherServicesSetam="➡️ Питання щодо інших послуг"
+)
+
+
+WhatIsSetam = dict(
+    Bidding='➡️Взяти участь у торгах',
+    FinanIss='➡️Фінансові питання',
+    VoluntarySale='➡️Добровільний продаж',
+    Familiarization='➡️Подати заявку на ознайомлення з майном'
+)
+
+
+Bidding = dict(
+    Registers='➡️Реєстрація на сайті',
+    ApplyForAuction='➡️Подати заявку на Аукціон'
+)
+
+Registers = dict(
+    RegForIndividuals='➡️Реєстрація для фізичних осіб',
+    RegForLegalEntities='➡️ Реєстрація для юридичних осіб',
+    EdPersonalInfo='➡️ Редагування особистих даних',
+    ProblemsWithReg='➡️ Проблеми з реєстрацією'
+)
+
+
+FinanIss = dict(
+    Individuals='➡️Для фізичних осіб',
+    Entities='➡️Для юридичних осіб',
+    Remuneration='➡️Додаткова Винагорода',
+    WarrantyСontribution='➡️Повернення гарантійного внеску',
+    ReasonsForReturn='➡️Підстави повернення гарантійного внеску'
+)
+
+
+OtherServicesSetam = dict(
+    Marriage='➡️Шлюб за добу',
+    FreeTransfer='➡️Безоплатна передача'
+)
